9b6027f7df1a07c1ccd5fd8c400938ae40da5ab4
Valdir Castro <valdir.a.c.junior@gmail.com>
2025-10-14 00:32:25 +0100

    Add: Category and Product models

---
+++ ProductCatalogApi/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace ProductCatalogApi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+}
